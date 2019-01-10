96cdf1186454c2e37ca30b3038a2c510b47582e6
Nicolas Nunez <nicolasnunezromay@gmail.com>
2019-01-10 00:03:44 -0600

    Removed some credentials

--- conf/twilio/send.sh
+++
@@ -1,9 +0,0 @@
-. ~/AutomationScripts/conf/twilio/whatsthetwilio.properties
-acct=${sid}
-token=${authtoken}
-echo ${acct} ${token}
-curl -X POST https://api.twilio.com/2010-04-01/Accounts/${acct}/Messages.json \
---data-urlencode "From=+15127649985" \
---data-urlencode "Body=${1}" \
---data-urlencode "To=+15129980328" \
--u ${acct}:${token}
